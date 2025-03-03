760a8c3e7e21848b5d36d40831940f697426f6d7
Junior <1108211033@gap.wzu.edu.tw>
2025-03-03 13:53:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,5 +6,11 @@
         <title>PHP & mysql</title> 
 </head>
 <body>
+    <h1>我的練習 php & mysql</h1>
+    <hr>
+    <p>今天是: <?= date("Y年M月D日")?></p>
+    <p>我的身高: <?= $h=178 ?></p>
+    <p>我的體重: <?= $w=67 ?></p>
+    <p>我的BMI: <?= $w/($h/100*$h/100) ?></p>
 </body>
 </html>
