eedb5254e336698b0ef2aa9145e5e6450c58fc55
Junior <1108211033@gap.wzu.edu.tw>
2025-03-03 13:49:22 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewpert" content="width=device-width, initial-scale=1.0">
+        <title>PHP & mysql</title> 
+</head>
+<body>
+</body>
+</html>
