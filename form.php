e5d02baef2f9c06a0d63bbe69d50f789cfcd1854
malicuious <1108211033@gap.wzu.edu.tw>
2025-03-10 14:34:18 +0800

    Create form.php

---
+++ form.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewpert" content="width=device-width, initial-scale=1.0">
+        <title>活動報名表</title> 
+</head>
+<body>
+    <h1>活動報名表</h1>
+    <form action="">
+        <fieldset>
+            <legend>基本資料</legend>
+            <p>姓名<input type="text" name="name" id="name"></p>
+        </fieldset>
+
+        <fieldset>
+            <legend>使用行為</legend>
+            <p>姓名<input type="text" name="name" id="name"></p>
+        </fieldset>
+
+        <fieldset>
+            <legend>滿意度</legend>
+            <p>姓名<input type="text" name="name" id="name"></p>
+        </fieldset>
+
+        <fieldset>
+            <legend>資料上傳</legend>
+            <p>姓名<input type="text" name="name" id="name"></p>
+        </fieldset>
+
+        
+    </form>
+</body>
+</html>
