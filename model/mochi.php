488e6878d8d802d521771fc2759e7f9624560f9f
malicious <1108211033@gap.wzu.edu.tw>
2025-05-05 16:00:26 +0800

    Update mochi.php

--- model/mochi.php
+++ model/mochi.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+
+function loginOK() {
+    return (isset($_SESSION["loggedin"]) && ($_SESSION["loggedin"]===true));
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,17 +17,22 @@
     <h1>彈出式對話視窗</h1>
 
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#loginModal">
-    Launch demo modal
-</button>
+
+<?php if (loginOK()) { ?>
+        <?= $_SESSION["username"]; ?>
+        <a class="btn btn-success" href="#" id="logout">Logout</a>
+<?php } else { ?>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#loginModal">
+            登入管理
+        </button>
+<?php } ?>
+
+
 
 
 <!-- Modal -->
-
 <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-
     <div class="modal-dialog">
-
         <div class="modal-content">
 
             <div class="modal-header">
@@ -47,14 +59,71 @@
             </div>
 
         </div>
-
     </div>
-    
 </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"></script>
-</body>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- 透過 CDN 載入 jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<script>
+$(document).ready(function () {
+
+    // 執行登入認證
+    $('#login_button').click(function () {
+
+        // 取出登入表單中，使用者帳號密碼的輸入值
+        var username = $('#username').val();
+        var userpass = $('#userpass').val();
+
+        // alert("username"+username+ " userpass"+userpass);
+
+        if (username != '' && userpass != '') {
+            $.ajax({
+                url: "action.php",
+                method: "POST",
+                data: {
+                    "action": "login",
+                    "username": username,
+                    "userpass": userpass
+                },
+
+                success: function (data) {
+                    if (data == 'Yes') {
+                        location.reload();
+                        alert("成功登入系統...");
+                    } else {
+                        // location.reload();
+                        alert('帳密無法使用!');
+                    }
+                },
+
+                error: function (data) {
+                    alter('無法登入');
+                }
+            });
+        } else {
+            alert("兩個欄位都要填寫!");
+        }
+    });
+
+    // 執行登出
+    $('#logout').click(function () {
+        $.ajax({
+            url: "action.php",
+            method: "POST",
+            data: {
+                "action": "logout",
+            },
+            success: function () {
+                location.reload();
+                alert("您已登出本系統...");
+            }
+        });
+    });
+});
+</script>
+
 </body>
 </html>
